0075f3faf3f3131be4b6f2eef31e992f251d91b4
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-04-09 09:52:56 -0400

    Add constructors for BSTMap

--- src/BSTMap.java
+++ src/BSTMap.java
@@ -1,3 +1,5 @@
+import java.util.Comparator;
+
 /**
  * Author: Muneeb Azfar Nafees
  * 
@@ -9,15 +11,54 @@ public class BSTMap<K, V> implements MapSet<K, V>{
     private Node<K,V> root;
     private int size;
     private Comparator<K> comparator;
-    
-    private static class Node<K, V> extends KeyValuePair<K, V>{
-        Node<K, V> left;
-        Node<K, V> right;
 
+    /**
+     * Adds a key-value pair to the map.
+     * If the key already exists, it updates the value.
+     * If the value is null, it adds the key with a the given value.
+     */
+    private static class Node<K, V> extends KeyValuePair<K, V>{
+        private Node<K, V> left;
+        private Node<K, V> right;
+
+        /**
+         * Constructor for the Node class.
+         * @param k the key
+         * @param v the value
+         */
         public Node(K k, V v) {
             super(k, v);
             left = null;
             right = null;
         }
+    }
+
+    /**
+     * Constructor for the BSTMap class.
+     * @param comparator the comparator to be used for sorting the keys
+     */
+    public BSTMap(Comparator<K> comparator){
+        root = null;
+        size = 0;
+
+        if(comparator != null){
+            this.comparator = comparator;
+        }
+        else{
+            this.comparator = new Comparator<K>(){
+                @Override
+                public int compare(K o1, K o2) {
+                    return ((Comparable<K>) o1).compareTo(o2);
+                }
+            };
+        }
+    }
+
+    /**
+     * Default constructor for the BSTMap class.
+     * This constructor uses the comparator as null.
+     */
+    public BSTMap(){
+        this(null);
     }
 }
