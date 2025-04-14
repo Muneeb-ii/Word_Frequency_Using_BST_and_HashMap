76ae8a8833e8a60f8f3423c8b2181819f7fbd146
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-04-13 23:56:25 -0400

    Add fields and constructors

--- src/HashMap.java
+++ src/HashMap.java
@@ -6,6 +6,10 @@
 
 public class HashMap<K,V> implements MapSet<K,V> {
 
+    private int size;
+    private Node<K,V>[] nodes;
+    private double maxLoadFactor;
+
     /**
      * Creates a node using the given key and value.
      */
@@ -21,6 +25,38 @@ public class HashMap<K,V> implements MapSet<K,V> {
             super(k, v);
             next = null;
         }
+    }
+
+    /**
+     * Constructor for the HashMap class.
+     * Initializes the size to 0 and the nodes array to a default size of 16.
+     */
+    public HashMap(){
+        this(16);
+    }
+
+    /**
+     * Constructor for the HashMap class.
+     * Initializes the size to 0 and the nodes array to the given capacity.
+     * 
+     * @param capacity the initial capacity of the HashMap
+     */
+    public HashMap(int capacity){
+        size = 0;
+        nodes = new Node[capacity];
+    }
+
+    /**
+     * Constructor for the HashMap class.
+     * Initializes the size to 0, the nodes array to the given capacity,
+     * and the max load factor to the given load factor.
+     * 
+     * @param capacity   the initial capacity of the HashMap
+     * @param loadFactor the maximum load factor of the HashMap
+     */
+    public HashMap(int capacity, double loadFactor){
+        this(capacity);
+        this.maxLoadFactor = loadFactor;
     }
 
 }
